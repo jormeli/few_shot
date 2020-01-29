6d9c5b32c3f8ebcc7b599737a75359f9b58e5f33
Aleksi Tella <aleksi.tella@gmail.com>
2020-01-29 11:45:19 +0200

    Add augmentations to train, freeze layers option

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 from datetime import datetime
 import math
 import os
@@ -9,12 +10,14 @@ import sys
 import gin
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import transforms, models
 from tqdm import tqdm
 
-from dataset import FashionDataset, Rescale, ToTensor, Normalize
+from dataset import FashionDataset, Resize, ToTensor, Normalize, RandomAugment
+
 
 @gin.configurable
 def train(experiment_dir,
@@ -24,17 +27,32 @@ def train(experiment_dir,
           save_every=1,
           val_split=0.2,
           data_root='./data',
-          csv_file='./train_top20.csv'):
+          csv_file='./train_top20.csv',
+          freeze_conv_layers=False,
+          weight_decay=0.,
+          augment=False,
+          model_path=None):
     """Create model, run training and save checkpoints."""
 
-    transform = transforms.Compose([Rescale((224, 224)),
+    augmentations = [
+            transforms.ColorJitter(.3, .3, .3, .3),
+            transforms.RandomGrayscale(0.1),
+            transforms.RandomHorizontalFlip(0.5),
+            transforms.RandomVerticalFlip(0.5),
+            transforms.RandomRotation(90),
+    ]
+
+    augment_p = 0.8 if augment else 0.0
+    transform = transforms.Compose([
+                                    RandomAugment(augmentations, p=augment_p),
+                                    Resize((224, 224)),
                                     ToTensor(),
                                     Normalize(
                                         # Mean and STD for pretrained model
                                         mean=[0.485, 0.456, 0.406],
                                         std=[0.229, 0.224, 0.225]
                                     )])
-    dataset = FashionDataset(csv_file='./train_top20.csv',
+    dataset = FashionDataset(csv_file=csv_file,
                              classes_file='./classes.txt',
                              root_dir=data_root,
                              transform=transform)
@@ -46,8 +64,22 @@ def train(experiment_dir,
             [len(dataset) - val_set_len, val_set_len]
     )
 
-    model = models.resnet50(pretrained=True)
-    model.fc = nn.Linear(2048, dataset.num_classes)
+    if not model_path:
+        model = models.resnet50(pretrained=True)
+        model.fc = nn.Linear(2048, dataset.num_classes)
+    else:
+        model = models.resnet50(pretrained=False)
+        if freeze_conv_layers:
+            for child in list(model.children())[:-3]:
+                for param in child.parameters():
+                    param.requires_grad = False
+        model.fc = nn.Linear(2048, dataset.num_classes)
+        model.load_state_dict(torch.load(model_path))
+
+
+    print('-' * 20)
+    print(model)
+    print('-' * 20)
 
     train_loader = DataLoader(train_set, batch_size=batch_size,
                               shuffle=True, num_workers=12)
@@ -59,9 +91,10 @@ def train(experiment_dir,
     datasets = {'train': train_set, 'val': val_set}
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     model = model.to(device)
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=lr)
+    optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
     step_history = []
     acc_history = []
@@ -70,6 +103,7 @@ def train(experiment_dir,
     # Training loop
     for epoch in range(1, num_epochs + 1):
         print('Epoch {}/{}'.format(epoch, num_epochs))
+        print('-' * 20)
 
         # Run train and val for each epoch
         for phase in ['train', 'val']:
@@ -119,6 +153,7 @@ def train(experiment_dir,
 
 
 
+        print('-' * 20)
         if epoch % save_every == 0:
             torch.save(model.state_dict(),
                        path.join(experiment_dir, 'weights',
@@ -128,6 +163,7 @@ def train(experiment_dir,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', help='Configuration file')
+    parser.add_argument('-m', '--model', help='Model path')
     args = parser.parse_args()
 
     experiment_name = '{0:%Y-%m-%d-%H-%M-%S}'.format(
@@ -141,4 +177,4 @@ if __name__ == '__main__':
         copyfile(args.config, path.join(experiment_dir, 'config.gin'))
         gin.parse_config_file(args.config)
 
-    train(experiment_dir)
+    train(experiment_dir, model_path=args.model)
